ff4ce8cbdb9eabbd86b96f399818483ab8d84f7a
Zun <jeff5566g@gmail.com>
2022-05-18 11:55:44 +0800

    新增items資料表

--- part/javascript_for_pay.php
+++ part/javascript_for_pay.php
@@ -1,25 +1,4 @@
     <script src="js/jquery-3.6.0.js"></script>
-    <script>
-        $(".z_checkbox_container :checkbox").click(function(){
-            $(this).parent().siblings().find(":checkbox").prop('checked', false);
-            if ($("#payBank").prop('checked')) {
-                $(".bank_area").css("display","block")
-                $(".card_area").css("display","none")
-
-            } else if ($("#payCard").prop('checked')) {
-                $(".bank_area").css("display","none")
-                $(".card_area").css("display","block")
-
-            } else {
-                $(".bank_area").css("display","none")
-                $(".card_area").css("display","none")
-            }
-        })
-
-        $("#bankSelect").click(function(){
-            $(".arrow_container img").toggleClass("downArrow")
-        })
-
-    </script>
+    <script src="pay/pay.js"></script>
     </body>
 </html>
