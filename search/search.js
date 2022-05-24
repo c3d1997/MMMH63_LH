6db9757542e862e1bf3189aa6fa8e748d5acf990
c3d1997 <c3d1997@gmail.com>
2022-05-24 12:30:01 +0800

    首頁/搜尋/收藏js完成

--- search/search.js
+++ search/search.js
@@ -47,65 +47,8 @@ $('.pc-button-FEAC00-272').click(function() {
     })
 
 })
-$(".S-lg-share").mousedown(function() {
-    $(".S-lg-share").css({
-        'transform': 'translateY(4px)scale(1.1)'
 
-    })
-})
-$(".S-lg-share").mouseup(function() {
-    $(".S-lg-share").css({
-        'transform': 'translateY(0px)scale(1.1)'
-
-    })
-
-})
-$(".S-lg-share").mouseenter(function() {
-    $(".S-lg-share").css({
-        'transform': 'translateY(0px)scale(1.1)'
-
-    })
-
-})
-$(".S-lg-share").mouseleave(function() {
-    $(".S-lg-share").css({
-        'transform': 'translateY(0px)scale(1.0)'
-
-    })
-
-})
-
-
-
-$(".S-lg-like").mousedown(function() {
-    $(this).css({
-        'transform': 'translateY(4px)scale(1.1)'
-
-    })
-
-})
-$(".S-lg-like").mouseup(function() {
-    $(this).css({
-        'transform': 'translateY(0px)scale(1.1)'
-
-    })
-
-})
-$(".S-lg-like").mouseenter(function() {
-    $(this).css({
-        'transform': 'translateY(0px)scale(1.1)'
-
-    })
-
-})
-$(".S-lg-like").mouseleave(function() {
-        $(this).css({
-            'transform': 'translateY(0px)scale(1.0)'
-
-        })
-
-    })
-    // 
+// 
 $(".x-button-only").click(function() {
     $(this).toggleClass('x-tag-on-click');
     $(this).siblings().removeClass('x-tag-on-click')
@@ -116,4 +59,71 @@ $(".x-button-more").click(function() {
 })
 $(".x-button-clear").click(function() {
     $('button').removeClass('x-tag-on-click')
+})
+
+// 
+
+
+if (window.innerWidth > 376) {
+    $(".S-lg-share").mousedown(function() {
+        $(this).css({
+            'transform': 'translateY(0px)scale(1.0)'
+
+        })
+    })
+    $(".S-lg-share").mouseup(function() {
+        $(this).css({
+            'transform': 'translateY(0px)scale(1.1)'
+
+        })
+
+    })
+    $(".S-lg-share").mouseenter(function() {
+        $(this).css({
+            'transform': 'translateY(0px)scale(1.1)'
+
+        })
+
+    })
+    $(".S-lg-share").mouseleave(function() {
+        $(this).css({
+            'transform': 'translateY(0px)scale(1.0)'
+
+        })
+
+    })
+    $(".S-lg-like").mousedown(function() {
+        $(this).css({
+            'transform': 'translateY(0px)scale(1.0)',
+
+        })
+
+    })
+    $(".S-lg-like").mouseup(function() {
+        $(this).css({
+            'transform': 'translateY(0px)scale(1.1)',
+
+        })
+
+    })
+    $(".S-lg-like").mouseenter(function() {
+        $(this).css({
+            'transform': 'translateY(0px)scale(1.1)'
+
+        })
+
+    })
+    $(".S-lg-like").mouseleave(function() {
+        $(this).css({
+            'transform': 'translateY(0px)scale(1.0)'
+
+        })
+
+    })
+}
+$(".S-lg-svg").click(function() {
+    console.log(1);
+    $(this).toggleClass('x-svg-color')
+
+
 })
